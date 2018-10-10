46cde74ab877b282e954da2cf5f24b79027e28f8
John Chen <z5165118@student.unsw.edu.au>
2018-10-10 21:43:08 +1100

    new file:   collection.txt 	new file:   readData.c 	new file:   url11.txt 	new file:   url21.txt 	new file:   url22.txt 	new file:   url23.txt 	new file:   url31.txt 	new file:   url32.txt 	new file:   url34.txt

---
+++ readData.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "graph.h"
+#include "set.h"
+
+
+
+
+Set list = newSet()
+	Graph web = newGraph(maxURLs);
