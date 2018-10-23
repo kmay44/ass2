134c81ba226f1fefab6094f092bcf2d41e859ec5
John Chen <z5165118@student.unsw.edu.au>
2018-10-23 18:11:11 +1100

    modified:   p3/scaledFootrule.c

--- p3/scaledFootrule.c
+++ p3/scaledFootrule.c
@@ -4,10 +4,12 @@
 #include <string.h>
 #include <math.h>
 
-    typedef struct rank {   
-	    char str[128][128];
-	    int size;
-    } rank ;
+typedef struct rank {   
+    char str[128][128];
+    int size;
+} rank ;
+
+
 void swap(int *a, int *b);
 void printArray(int *array, int length);
 int notInList(rank r, char *s);
@@ -27,8 +29,8 @@ int main(int argc, char *argv[])
     int nUrls =0 ;
     int nUrls1=0;
     char line[1024];
-    char url[3][1024][1024];
     int files =1;
+    
     for (i = 1; i < argc; i++) {
         nUrls1=0;
         FILE *stream = fopen(argv[i], "r");
@@ -47,24 +49,17 @@ int main(int argc, char *argv[])
         fclose(stream);
     }
     files = j;
-    for (j = 0; j < argc; j++) {
+    
+ //   for (j = 0; j < argc; j++) {
  //   printf("c\n");
-        for (k = 0; k < ranks[j].size; k++) {
+ //       for (k = 0; k < ranks[j].size; k++) {
        //     printf("%s\n", ranks[j].str[k]);
-        }
-    }
-  //  url[0][nUrls][0] = '\0';
-  // for (i = 0; i < j; i++)
- //   printArrayS(url[i], j);
-   // printf("%d\n", nUrls);
+ //       }
+ //   }
+
     permutation(array, nUrls);
 
-  //  array[0] = 1;
-  //  array[1] = 3;
- //   array[2] = 2;
-  //  array[3] = 5;
-  //  array[4] = 4;
- //   printf("%lf", scaledFootrule(array, ranks, files));
+
     double total = 100;
     total = permute(array, 0, nUrls, ranks, files, total, optarray);
     printf("%lf\n", total);
@@ -102,13 +97,13 @@ double scaledFootrule(int *array, rank ranks[], int files) {
             n = ranks[0].size;
             if (found) {
             total+=fabs(pos/fn - a/n);
-       //     printf("%lf = abs(%lf/%lf - %lf/%lf)    ", total, pos, fn, a, n);
+
             }
             
         }
-     //   printf("\n");
+
     }
- //   printf("\n");
+
     return total;
 }
         
@@ -118,15 +113,12 @@ double permute(int *array,int i,int length, rank ranks[], int files, double tota
     if (length == i){
         double dist = scaledFootrule(array, ranks, files);
         if (dist < total){
-       //     printf("%lf < %lf\n", dist , total);
-        //    total = dist;
-       //     printf("%lf\n", total);
-      //  printf("%lf\n\n", total);
+
             int k;
             for (k = 0; k < length; k++) {
                 optarray[k] = array[k];
             } 
-       //     printArray(array,length);
+ 
             return dist;
         }
         else return total;
@@ -146,7 +138,7 @@ void permutation(int *array, int len)
 {
     int i;
     for (i = 0; i < len; i++) {
-  //      printf("%d\n", i);
+
         array[i] = i + 1;
     }
 }
